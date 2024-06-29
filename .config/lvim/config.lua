ce133c1f48694e2309ff3fbdf8ae4911b49130ee
almond Heil <almond@almendra.dev>
2024-06-28 20:56:50 -0600

    lvim: basic theme settings

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -3,3 +3,14 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.plugins = {
+  {
+    'chriskempson/base16-vim',
+    priority = 1000, -- Ensure it loads first
+  },
+}
+
+lvim.builtin.lualine.style = "default"
+
+lvim.colorscheme = "base16-outrun-dark"
